b863336cdd187ee7cd0790193c0a3ada54a6988c
Daserix <maximilian.domschke@googlemail.com>
2024-10-25 15:58:02 +0200

    finish excercise

--- Modul_JavaScript/destruct.js
+++ Modul_JavaScript/destruct.js
@@ -10,3 +10,17 @@
 // let x2 = points[1]["x"];
 // let y2 = points[1]["y"];
 // console.log(x1 === 1 && y1 === 2 && x2 === 3 && y2 === 4)
+
+
+// let points = { p1: [1,2], p2: [3,4] };
+// let { p1: [x1, y1], p2: [x2, y2] } = points;
+// console.log(x1 === 1 && y1 === 2 && x2 === 3 && y2 === 4)
+
+// entspricht folgendem:
+
+let points = { p1: [1, 2], p2: [3, 4] };
+let x1 = points["p1"][0];
+let y1 = points["p1"][1];
+let x2 = points["p2"][0];
+let y2 = poinst["p2"][1];
+console.log(x1 === 1 && y1 === 2 && x2 === 3 && y2 === 4)
